8c43e11f058fcb57602b2aac2923e18a7776b18a
khanh178 <tdkhanh178@gmail.com>
2016-10-17 15:38:11 +0700

    lên

---
+++ application/controllers/admin/store_accounts.php
@@ -0,0 +1,29 @@
+<?php
+class Store_accounts extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
+	function manage() {
+		$this->_is_admin();
+		$data['view_file'] = "admin/manage";
+		$this->admin($data);
+	}
+
+	function admin($data) 
+	{	
+		$this->load->view('admin/admin', $data);
+	}
+
+	function _is_admin() {
+	$is_admin = TRUE;
+	if ($is_admin != TRUE) {
+			redirect('admin/store_items/not_allowed');
+		}
+	}
+
+	function not_allowed() {
+		echo "You are not allowed here.";
+	}
+}
---
+++ application/views/admin/manage.php
@@ -0,0 +1,20 @@
+<h1>Manage Accounts</h1>
+
+<?php
+$create_account_url = base_url()."index.php/admin/store_accounts/create";
+?>
+
+<p style="margin-top: 30px;">
+<a href="<?= $create_account_url ?>"><button type="button" class="btn btn-primary">Create New Account</button></a>
+</p>
+
+<table class="table">
+	<tr>
+		<td>A</td>
+		<td>A</td>
+		<td>A</td>
+		<td>A</td>
+		<td>A</td>
+		<td>A</td>
+	</tr>
+</table>
